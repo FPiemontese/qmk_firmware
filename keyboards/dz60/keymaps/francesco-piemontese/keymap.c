277c24f2a214863af244f21d58f37e7e0057b285
Francesco Piemontese <piemontesefrancesco@outlook.com>
2021-05-23 10:05:55 +0200

    update keymap visualizations

--- keyboards/dz60/keymaps/francesco-piemontese/keymap.c
+++ keyboards/dz60/keymaps/francesco-piemontese/keymap.c
@@ -10,26 +10,22 @@ TODOs:
   - add a non-temporary layer switch key
   - add an FN on the left
     - (DONE) maybe have CAPS lock act as itself when tapped and as FN when held down
-  - remove unused RGB commands
 
 MAYBEs:
-  - add a numpad
-  - move arrow keys to the left (FN + WASD?), have backspace and del on the right instead (removes
-    need to move hand from arrow keys to backspace when navigating code)
+  - add a numpad (would have to be in an additional layer)
 
 */
-
 
 // DZ60 layout 5, 1u right shift, arrow keys, 5 1u keys in the bottom row
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------------------------------------------------------.
-   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bkspc   |
+   * |ESC/~|  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bkspc   |
    * |-----------------------------------------------------------------------------------------+
    * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
    * |-----------------------------------------------------------------------------------------+
-   * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
+   * | Caps/FN |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
    * |-----------------------------------------------------------------------------------------+
    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | DEL |
    * |-----------------------------------------------------------------------------------------+
@@ -47,13 +43,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* FN Layer
    * ,-----------------------------------------------------------------------------------------.
-   * | Esc |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |           |
+   * |  `  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |           |
    * |-----------------------------------------------------------------------------------------+
-   * |        |RBB T|RGB M| Hue+| Hue-|     |     |     |  U  |     |     |      |      | RESET|
+   * |        |     |  ↑  |     |     |     |     |     |  ↑  |     |     |      |      | RESET|
    * |-----------------------------------------------------------------------------------------+
-   * |         | Sat+| Sat-| Val+| Val-|     |     |  L  |  D  |  R  |     |  `  |             |
+   * |         |  ←  |  ↓  |  →  |     |     |     |  ←  |  ↓  |  →  |     |  `  |             |
    * |-----------------------------------------------------------------------------------------+
-   * |           |     |     |     |     |     |     |     |     |     | STOP| ▶|| |VOL↑ |     |
+   * |           |RGB T|RGB M|Hue ±|Sat ±|Val ±|     |     |     |     |STOP | ▶|| |VOL↑ |     |
    * |-----------------------------------------------------------------------------------------+
    * |      |       |       |                                   |     |      |TRCK←|VOL↓ |TRCK→|
    * `-----------------------------------------------------------------------------------------'
@@ -61,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // RGB_HUD can be accessed by pressing Shift + RGB_HUI. The same applies for RGB_SAD and RGB_VAD
   LAYOUT_directional(
-    KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, ______, ______,
+    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, ______, ______,
     ______, ______, KC_UP, ______, ______, ______, ______, ______,  KC_UP, ______, ______, ______, ______, RESET,
     ______, KC_LEFT, KC_DOWN, KC_RIGHT, ______, ______, ______, KC_LEFT, KC_DOWN, KC_RIGHT, ______, KC_GRV, ______,
     ______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, ______, ______, ______, ______, KC_MSTP, KC_MPLY, KC_VOLU, ______,
