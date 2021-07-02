a1d5a4b3c3b81728a5ceba853f5a388dbb94d062
Francesco Piemontese <f.piemontese@reply.it>
2021-07-02 11:36:04 +0200

    add right click

--- keyboards/dz60/keymaps/francesco-piemontese/keymap.c
+++ keyboards/dz60/keymaps/francesco-piemontese/keymap.c
@@ -3,14 +3,11 @@
 #define ______ KC_TRNS
 
 /*
-
 TODOs:
-  - add a right click
   - add a non-temporary layer switch key
 
 MAYBEs:
   - add a numpad (would have to be in an additional layer)
-
 */
 
 // Tap-dance declarations
@@ -72,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ______, ______, KC_UP, ______, ______, ______, ______, ______,  KC_UP, ______, ______, ______, ______, RESET,
     ______, KC_LEFT, KC_DOWN, KC_RIGHT, ______, ______, ______, KC_LEFT, KC_DOWN, KC_RIGHT, ______, KC_GRV, ______,
     ______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, ______, ______, TD(TD_HOME_PAGEUP), TD(TD_END_PAGEDOWN), KC_MSTP, KC_MPLY, KC_VOLU, ______,
-    KC_RCTL, ______, ______, ______, ______, ______, ______, ______, KC_MPRV, KC_VOLD, KC_MNXT
+    KC_RCTL, ______, ______, ______, ______, ______, KC_MS_BTN2, ______, KC_MPRV, KC_VOLD, KC_MNXT
   ),
 
 };
